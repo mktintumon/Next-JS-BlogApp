9248b61df3e3c07fe84a05ecca26ce8d473e8926
mktintumon <mktintumon@gmail.com>
2024-02-06 14:59:18 +0530

    frontend completed

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,8 +1,54 @@
-export default function SinglePostPage() {
-    return (
-        <div>
-            SinglePostPage
-        </div>
-    );
-}
+import Image from "next/image";
+import styles from "./singlePost.module.css";
+import PostUser from "@/components/postUser/postUser";
+import { Suspense } from "react";
 
+// FETCH DATA WITH AN API
+const getData = async (slug) => {
+  const res = await fetch(`https://dummyjson.com/posts/${slug}`);
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
+const SinglePostPage = async ({ params }) => {
+  const { slug } = params;
+
+  // FETCH DATA WITH AN API
+  const post = await getData(slug);
+
+  return (
+    <div className={styles.container}>
+      
+        <div className={styles.imgContainer}>
+          <Image
+            src="https://images.pexels.com/photos/4549414/pexels-photo-4549414.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+            fill
+            className={styles.img}
+          />
+        </div>
+      
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <div className={styles.detail}>
+          {post && (
+            <Suspense fallback={<div>Loading user...</div>}>
+              <PostUser userId={post.userId} />
+            </Suspense>
+          )}
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>30.10.1999</span>
+          </div>
+        </div>
+        <div className={styles.content}>{post.body}</div>
+      </div>
+    </div>
+  );
+};
+
+export default SinglePostPage;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,33 @@
-export default function Blog() {
-    return (
-        <div>
-            blog
-        </div>
-    );
-}
+import PostCard from "@/components/postCard/postCard";
+import styles from "./blog.module.css";
+//import { getPosts } from "@/lib/data";
 
+// FETCH DATA WITH AN API
+const getData = async () => {
+  const res = await fetch("https://dummyjson.com/posts", {next:{revalidate:3600}});
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
+const BlogPage = async () => {
+
+  // FETCH DATA WITH AN API
+  const posts = await getData();
+  
+
+  return (
+    <div className={styles.container}>
+      {posts.posts.map((post) => (
+        <div className={styles.post} key={post.id}>
+          <PostCard post={post} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogPage;
